851bda636007cd4cbb8c7b18798359ace8e332b1
y-suzumura-winas <y-suzumura@winas.jp>
2019-02-12 16:35:41 +0900

    [fix]24

--- 3章/24. ファイル参照の抽出.py
+++ 3章/24. ファイル参照の抽出.py
@@ -15,7 +15,7 @@ import re
 
 
 def read_wiki(fname, tiltle):
-    with gzip.open(fname, 'rt') as data_file:
+    with gzip.open(fname, 'rt', encoding='UTF-8') as data_file:
         for line in data_file:
             data_json = json.loads(line)
             if data_json['title'] == 'イギリス':
@@ -23,25 +23,35 @@ def read_wiki(fname, tiltle):
 
 
 def is_file(string):
-    return re.match(r'^\[\[(ファイル|File):.*\]\]$', string)
+    return re.match(r'\[\[:*(ファイル|File):.*\]\]$', string)
 
 
 def get_file_name(string):
-    m = re.match(r'^\[\[(ファイル|File):(.+?)\|.+\]\]$', string)
+    m = re.match(r'\[\[:*(ファイル|File):(.+?)\|.+\]\]$', string)
     # m.group(1) => '==='
     return m.group(2)
 
 
+# ギャラリーに対応
+def is_gallery_file(string):
+    return re.match(r'^(ファイル|File):.*\]\]$', string)
+
+
+def get_gallery_file(string):
+    m = re.match(r'^(ファイル|File):(.+?)\|.+$', string)
+    return m.group(2)
+
 def main():
     fname = 'jawiki-country.json.gz'
     text = read_wiki(fname, 'イギリス').split('\n')
-    print(len(text))
 
     # カテゴリの行を表示する
     for line in text:
+        # print(line)
         if is_file(line):
-            # print(line, end=' : ')
             print(get_file_name(line))
+        elif is_gallery_file(line):
+            print(get_gallery_file(line))
 
 
 if __name__ == '__main__':
